bb2d09f4db51683fe9de1ac88fb227fe2295a215
Kiran6248 <kiran.satyarthy@gmail.com>
2020-12-29 19:32:52 +0000

    Add comments to the pokemon.js file

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,4 +1,6 @@
 //function to show pokemon information and picture
+//jQuery code to select pokemon from dropdown box
+
 $(document).on('change', '.poke-selector', function() {
     var target = $(this).data('target');
     var show = $("option:selected", this).data('show');
