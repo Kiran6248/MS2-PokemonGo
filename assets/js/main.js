0963018615a837dd470dde3fc7ade301d0606bac
Kiran6248 <kiran.satyarthy@gmail.com>
2020-12-30 17:18:11 +0000

    add comments in the main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,11 +22,11 @@ const getMoves = localStorage.getItem('moves') + 1
 //Function when the card is flipped, checks to see if the card has already been flipped, if its the first card and adds the class flip.
 //then runs the match againest the second card flip and increase the move counter which are both separate function that are called
 
-function flipCard() {
-    if (lockBoard) return;
+function flipCard(){
+    if (lockboard) return;
     if (this === firstCard) return;
     this.classList.add('flip')
- if (!hasFlippedCard) {
+    if (!hasFlippedCard){
         hasFlippedCard = true;
         firstCard = this;
         return;
@@ -34,93 +34,92 @@ function flipCard() {
 
     secondCard = this;
     lockBoard = true;
-checkForMatch();
+    checkForMatch();
     moveCounter();
-
 }
+
 //check both cards for match
-function checkForMatch() {
+function checkForMatch(){
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCards() : unflipCards(); //cards can not be flip again
 }
-    function disableCards() {
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-        countCounter(); //increase the match counter
-        resetBoard(); //disable both cards
-        localStorage.setItem = ('moves')+1 //sets stored in local storage
-        congrats(); //when 8 matches have been completed bring up congrats modal
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    countCounter(); //increase the match counter
     
+    resetBoard(); //disable both cards
+    localStorage.setItem = ('moves')+1 //sets stored in local storage
+    congrats(); //when 8 matches have been completed bring up congrats modal
 }
- function unflipCards() {
-        lockBoard = true;
-        setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            resetBoard();
-        }, 1200);
-    }
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    },1200);
+}
 
-    function resetBoard() {
-        [hasFlippedCard, lockBoard] = [false, false];
-        [firstCard, secondCard] = [null, null];
-    }
-   (function shuffle() {
-        cards.forEach(card => {
-            let ramdomPos = Math.floor(Math.random() * 16); //math for shuffling the card
-            card.style.order = ramdomPos;
-        });
-    })();
- cards.forEach(card => card.addEventListener('click', flipCard));
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+(function shuffle() {
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 16); //math for shuffling the card
+        card.style.order = ramdomPos;
+    });
+})();
+cards.forEach(card => card.addEventListener('click', flipCard));
 
- function moveCounter(){ //moves counter increase by one for each match
+//moves counter by one for each match
+function moveCounter(){ 
     moves++;
     counter.innerHTML = moves;
-    if (moves > 14 && moves < 22){ //star ratings
+    if (moves > 14 && moves < 22){   //star ratings
         for (i = 0; i < 3; i++){
             if (i > 1){
                 stars[i].style.visibility = "collapse";
             }
         }
-    }else if (moves >23) {
-        for (i = 0; i < 3; i++){
-            if (i >0){
-                stars[i].style.visibility = "collapse";
-            }
-        }
-    }
-
-}
-function countCounter() { //count the matches
-    count++;
-    counter2.innerHTML = count;
+     }
 }
 
-function congrats() { //congrats modal display when matches = 8
-    if (count == 8) {
+//count the matches
+function countCounter(){ 
+      count++;
+      counter2.innerHTML = count;
+}
+
+// congrats modal displays when matches = 8
+function congrats(){
+    if (count == 8){
         modal.classList.add("show");
+       
 
-var starRating = document.querySelector(".stars").innerHTML;
-
- document.getElementById("finalMove").innerHTML = moves + 1;
+ var starRating = document.querySelector(".stars").innerHTML;
+ 
+ document.getElementById("finalMove").innerHTML = moves+1;
  document.getElementById("starRating").innerHTML = starRating;
 
-        closeModal() ;
+   closeModal();
     };
 }
 
-function closeModal(){ //close modal button
+// close modal button
+function closeModal(){
     closeicon.addEventListener("click", function(e){
         modal.classList.remove("show");
         restart();
     });
-
 }
-function restart() {  // reloads the page to reset all things
+
+//reloads the page to reset every thing
+function restart(){
     location.reload();
     return false;
-
 }
 cards.forEach(card => {
-        return card.addEventListener('click', flipCard);
-    });
+    return card.addEventListener('click', flipCard);
+});
